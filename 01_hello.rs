ddebdb796b167f44f0282a002db5ac401a802a8f
wccalvin <clayton@wccalvin.com>
2024-03-03 03:22:27 -0500

    add some more variants to hello world

--- 01_hello.rs
+++ 01_hello.rs
@@ -15,6 +15,9 @@ fn main() {
     println!("Hello World!"); // println! is a macro that prints text to console
     println!("I'm a Rustacean");
 
+    let str = "This is fun!";
+    println!("String: {}", str);
+
     let x = 5 + /*90 + */ 5;
     println!("value of x = {}", x);
 }
