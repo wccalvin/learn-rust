b48d0d8120e3603df4bf211337641fa7d250a1f4
wccalvin <clayton@wccalvin.com>
2024-03-03 03:09:38 -0500

    hello world

---
+++ 01_hello.rs
@@ -0,0 +1,10 @@
+// This is a comment, ignored by the compiler
+// To execute this program, first compile: rustc 01_hello.rs
+// rustc will produce a binary that can be executed
+
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiler binary is called
+    println!("Hello World!"); // println! is a macro that prints text to console
+    println!("I'm a Rustacean");
+}
